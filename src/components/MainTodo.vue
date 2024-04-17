f7fc5bbba8f09b0eee8b072a3ccbdf558c04f10d
Daisuke Ueda　上田 大輔 <daisuke.u524@gmail.com>
2024-04-18 07:34:44 +0900

    modify: ロジックを分離

--- src/components/MainTodo.vue
+++ src/components/MainTodo.vue
@@ -1,50 +1,33 @@
 <script setup>
   import { ref } from 'vue';
+  import { useTodoList } from '/src/composables/useTodoList.js';
+
   const todoRef = ref('');
-  const todoListRef = ref([]);
-  const ls = localStorage.todoList;
-  todoListRef.value = ls ? JSON.parse(ls) : [];
   const isEditRef = ref(false);
-  let editId = -1;
+  const { todoListRef, add, show, edit, del, check } = useTodoList();
 
   const addTodo = () => {
-    const id = new Date().getTime();
-    todoListRef.value.push({ id: id, task: todoRef.value });
-    localStorage.todoList = JSON.stringify(todoListRef.value);
+    add(todoRef.value);
     todoRef.value = '';
   };
 
   const showTodo = (id) => {
-    const todo = todoListRef.value.find((todo) => todo.id === id);
-    todoRef.value = todo.task;
+    todoRef.value = show(id);
     isEditRef.value = true;
-    editId = todo.id;
   };
 
   const editTodo = () => {
-    const todo = todoListRef.value.find((todo) => todo.id === editId);
-
-    const idx = todoListRef.value.findIndex((todo) => todo.id === editId);
-
-    todo.task = todoRef.value;
-
-    todoListRef.value.splice(idx, 1, todo);
-
-    localStorage.todoList = JSON.stringify(todoListRef.value);
+    edit(todoRef.value);
     isEditRef.value = false;
-    editId = -1;
     todoRef.value = '';
   };
 
   const deleteTodo = (id) => {
-    const todo = todoListRef.value.find((todo) => todo.id === id);
-    const idx = todoListRef.value.findIndex((todo) => todo.id === id);
+    del(id);
+  };
 
-    const delMsg = '「' + todo.task + '」を削除しますか？';
-    if (!confirm(delMsg)) return;
-
-    todoListRef.value.splice(idx, 1);
-    localStorage.todoList = JSON.stringify(todoListRef.value);
+  const changeCheck = (id) => {
+    del(id);
   };
 </script>
 
